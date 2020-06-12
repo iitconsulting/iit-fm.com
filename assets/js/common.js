eb9d630e09472ec08417302593e7372f7d45e432
iitconsulting <iitconsulting.fm@yandex.ru>
2020-06-12 19:13:40 +0300

    work in progress

---
+++ assets/js/common.js
@@ -0,0 +1,9 @@
+﻿function main() {
+	let headerNav = document.querySelector('header nav')
+	headerNav.querySelector('.close a').addEventListener('click', function () {
+		headerNav.classList.add('closed')
+	})
+	document.querySelector('header .nav-trigger').addEventListener('click', function () {
+		headerNav.classList.remove('closed')
+	})
+}
